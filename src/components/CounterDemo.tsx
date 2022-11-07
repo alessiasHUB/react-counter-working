52cb87d18f0ec6816622fdc0848226b1818e2afd
Alessia B <borys.alessia@gmail.com>
2022-11-07 17:38:49 +0100

    added more buttons

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -8,15 +8,25 @@ function CounterDemo(): JSX.Element {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
 
+  const handleSubOne = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+  };
+
   const handleReset = () => {
-    // TODO reset counter to 0 here
-  }
+    queueRerenderWithNewCounterValue(0);
+  };
+
+  
+
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
+      <button onClick={handleSubOne}>-1</button>
+      <button onClick={() => {queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5)}}>-5</button>
+      <button onClick={() => {queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5)}}>+5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
